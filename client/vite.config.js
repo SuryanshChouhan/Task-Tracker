e222c2997193a8461de8c7764eb5db749041f262
Suryansh Chouhan <suryanshchouhan20@gmail.com>
2026-05-03 12:51:29 +0530

    Modify Vite config for hosting and preview
    Updated Vite configuration to allow hosting and added preview settings.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    host: true
+  },
+  preview: {
+    host: true,
+    allowedHosts: [
+      "task-tracker-production-d2d1.up.railway.app"
+    ]
   }
-});
+})
